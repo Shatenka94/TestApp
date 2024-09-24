dccd372f4d4b24a0b07d7f57ac93b52acfa4a4ce
Aisuluu Shatenova <aisuluushatenova@Aisuluus-Laptop.local>
2024-09-23 20:45:49 -0500

    updated

--- src/test/java/ReverseTests.java
+++ src/test/java/ReverseTests.java
@@ -7,5 +7,16 @@ public class ReverseTests {
         Assert.assertNull(ReverseStringClass.reverseString(null));
         System.out.println("output passed");
     }
+    @Test
+    public void reverseTestWithDigit(){
+        Assert.assertEquals("cba3218",ReverseStringClass.reverseString("123abc"));
+        System.out.println("reverseTestWithDigit");
+    }
+
+    @Test
+    public void reverseTestHappyPath(){
+        Assert.assertEquals("elppa",ReverseStringClass.reverseString("apple"));
+        System.out.println("reverseTestHappyPathHello");
+    }
 
 }
